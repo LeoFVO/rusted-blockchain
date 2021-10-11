2336eef52c5fc00d6b4bec60bc4ddc321d5cc854
Léo <leodevsecops@gmail.com>
2021-10-11 14:33:57 +0200

    build(block): block creation string conversion for struct changed timestamp type working on hash function

--- src/block.rs
+++ src/block.rs
@@ -1,22 +1,33 @@
-use std::time::SystemTime;
+use std::fmt;
+use chrono::prelude::*;
+// use sha2::{Sha256, Digest};
 
-struct Block {
+pub struct Block {
   transactions: String, // array of 10 transaction
   prev_block_hash: String,
-  timestamp: SystemTime,
+  timestamp: DateTime<Utc>,
 }
-
+impl fmt::Display for Block {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}{}",self.transactions, self.prev_block_hash, self.timestamp.format("%Y-%m-%d_%H:%M:%S"))
+    }
+}
 impl Block {
-  fn new(transactions: String, prev_block_hash: String) -> Block {
+  pub fn new(transactions: String, prev_block_hash: String) -> Block {
     Block {
       transactions,
       prev_block_hash,
-      timestamp: SystemTime::now(),
+      timestamp: Utc::now(),
     }
   }
-  fn toString() -> String {}
 
-  fn getHash() -> String {
+  /**
+   * TODO: Fix type mismatch between .finalize() and String
+   */
+  pub fn getHash(&self) -> String {
     // SHA 256 hash of stringified struct
+    // let mut hasher = Sha256::new();
+    // hasher.update(format!("{}", self));
+    "defaulthash".to_string()
   }
 }
